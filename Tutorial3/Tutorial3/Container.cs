cc466e07937aad235ca9ed3c55620e60ae7f3f7b
s24693 <s24693@pjwstk.edu.pl>
2024-03-21 23:47:06 +0100

    Added Container, LiquidContainer, Exception and Interface

---
+++ Tutorial3/Tutorial3/Container.cs
@@ -0,0 +1,23 @@
+﻿namespace Tutorial3;
+
+public abstract class Container
+{
+    public double MassCargo { get; protected set; }
+    public int Height { get; private set; }
+    public double TareWeight { get; private set; }
+    public int Depth { get; private set; }
+    public string SerialNumber { get; private set; }
+    public double MaxPayload { get; private set; }
+
+    protected Container(double tareWeight, int height, int depth, string serialNumber, double maxPayload)
+    {
+        TareWeight = tareWeight;
+        Height = height;
+        Depth = depth;
+        SerialNumber = serialNumber;
+        MaxPayload = maxPayload;
+    }
+
+    public abstract void LoadCargo(double mass);
+    public abstract void EmptyCargo();
+}
---
+++ Tutorial3/Tutorial3/OverfillException.cs
@@ -0,0 +1,6 @@
+﻿namespace Tutorial3;
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message) {}
+}
